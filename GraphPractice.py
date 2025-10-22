143b92559e4eccc6f830cf25957449a8d50bcd14
zekeThree <ezekielsopchak@outlook.com>
2025-10-21 21:16:20 -0400

    removed NeuralNetworkAnimation added Graph practice

---
+++ GraphPractice.py
@@ -0,0 +1,32 @@
+from manim import *
+ 
+class NeuralNetworkAnimation(Scene):
+    def construct(self):
+        ax = Axes(x_range=[-10,10,2], y_range=[0,12,1])
+        ax2 = Axes(x_range=[-10,10,2], y_range=[0,12,1])
+        ax2.shift(UP*2)
+        animation1 = FadeIn(ax)
+        self.play(animation1)
+        self.wait(2)
+        # inputNeuronList = []
+        # numberOfInputNeurons = 5
+        # for i in range(numberOfInputNeurons):
+        #     inputNeuronList.append(Circle(radius=.2,color=BLUE))
+        # for i in range(len(inputNeuronList)):
+        #     inputNeuronList[i].shift(DOWN * i)
+        #     self.play(Create(inputNeuronList[i]))
+        
+        # #add hidden layer and graph
+        # circ = Circle(color=RED).shift(4*LEFT)
+        # dot = Dot(color=RED).move_to(circ.get_start())
+        # #rolling_circle = VGroup(circ, dot)
+        # trace = TracedPath(circ.get_start)
+        # #rolling_circle.add_updater(lambda m: m.rotate(-0.1))
+        # #self.add(trace, rolling_circle)
+        # self.play(circ.animate.shift(8*RIGHT).shift(DOWN*3), run_time=4, rate_func=linear)
+        # self.add(trace)   
+        # self.play(circ.animate.shift(UP*3),run_time = 3,rate_func = linear )
+        # self.wait(2)
+        
+
+            
